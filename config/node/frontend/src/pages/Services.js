3873e22138db27fa117c8fa9fcf644c5cfffd34e
kolug1 <filipjanowski13@gmail.com>
2025-12-10 09:27:31 +0100

    feat: add map component with open layers map

--- config/node/frontend/src/pages/Services.js
+++ config/node/frontend/src/pages/Services.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import {Button} from "@mui/material";
 
 function Services(props) {
-    return (
-        <div>Services</div>
+    return (<div>
+            <div>Services</div>
+            <Button
+                className="services__button"
+                variant='contained'
+                size='large'
+                component={Link}
+                to='/map'>
+                Przejź do mapy
+            </Button>
+        </div>
+
     );
 }
 
