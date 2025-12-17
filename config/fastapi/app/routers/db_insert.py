229767b1ec81360c63e03ed0c338d77ae6c656d3
kolug1 <filipjanowski13@gmail.com>
2025-12-17 13:02:53 +0100

    feat: nie dziala

--- config/fastapi/app/routers/db_insert.py
+++ config/fastapi/app/routers/db_insert.py
@@ -7,19 +7,29 @@ router_insert = APIRouter()
 
 
 def connect_to_db(db_name: str, db_user: str, db_password: str):
-    return create_engine(f"postgresql://{db_user}:{db_password}@postgis:5432/{db_name}")
+    return create_engine(
+        f"postgresql://{db_user}:{db_password}@postgis:5432/{db_name}"
+    )
 
 
 @router_insert.get("/insert_user")
 async def insert_user():
     try:
-        db_connection = connect_to_db(db_name, db_user, db_password)
-        sql_query = text("INSERT INTO users (name, posts, location) VALUES ('Piotrek', 5, 'Warszawa');")
+        db_connection = connect_to_db(db_name=db_name, db_user=db_user, db_password=db_password)
+
+        sql_query = text("""
+                         insert into users (name, posts, location)
+                         values ('Jan', 3, 'Gdańsk'); \
+                         """)
+
         with db_connection.connect() as conn:
             result = conn.execute(sql_query)
             conn.commit()
             print(result)
-    except Exception as e:
-        return {"error": f"Database connection failed: {str(e)}"}
 
-    return {"status": "User inserted successfully"}
+
+    except Exception as e:
+        print(e)
+        raise e
+
+    return {"statu": 1}
