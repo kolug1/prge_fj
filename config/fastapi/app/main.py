e6c019ccb01a97d7afacca5ff449defa6cc82a49
kolug1 <filipjanowski13@gmail.com>
2026-01-12 12:54:01 +0100

    feat: added dynamic request to backend

--- config/fastapi/app/main.py
+++ config/fastapi/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.routers.static_content import router
 from app.routers.db_insert import router_insert
+from app.routers.dynamic_content import router_get_users
 app = FastAPI(title="MapBook API")
 
 app.include_router(router, prefix="/app")
 app.include_router(router_insert, prefix="/app")
+app.include_router(router_get_users, prefix="/app")
