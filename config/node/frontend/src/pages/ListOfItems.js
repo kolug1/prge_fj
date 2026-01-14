6e4f2d6a47ff1a53a9e776c0b612eade4961fb52
kolug1 <filipjanowski13@gmail.com>
2026-01-14 12:24:23 +0100

    feat: added new wms to map window

--- config/node/frontend/src/pages/ListOfItems.js
+++ config/node/frontend/src/pages/ListOfItems.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import UserCard from "../components/UserCard";
 
 function ListOfItems(props) {
-    return (
-        <div>List of items</div>
-    );
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    console.log("działa");
+    fetch("http://localhost:10000/app/get_users")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setUsers(res.data);
+      });
+    console.log("działa", users);
+  }, []);
+  useEffect(() => {
+    console.log("Stan users zaktualizowany:", users);
+  }, [users]);
+
+  console.log("To jest users i jego zawartość", users.data);
+  return (
+    <div>
+      ListOfItems
+      <div>
+        {users?.map((user) => (
+          <UserCard key={user.id || Math.random()} user={user} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ListOfItems;
